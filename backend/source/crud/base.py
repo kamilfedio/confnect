a06f0546a3120d4d823902161f86d33ef1400bec
Kamil Fedio <kamilf827@gmail.com>
2024-08-18 15:43:11 +0200

    feature: create structure of crud

---
+++ backend/source/crud/base.py
@@ -0,0 +1,68 @@
+from abc import ABC, abstractmethod
+from sqlalchemy import Sequence
+
+from source.schemas.base import Base
+from sqlalchemy.ext.asyncio import AsyncSession
+
+class BaseCRUD(ABC):
+    @abstractmethod
+    async def get_all(self, pagination: tuple[int, int], session: AsyncSession) -> Sequence[Base]:
+        """ get all models from the database
+
+        Args:
+            pagination (tuple[int, int]): skip, limit
+            session (AsyncSession): session db
+
+        Returns:
+            Sequence[Base]: sequence of models
+        """
+        pass
+
+    @abstractmethod
+    async def get_by_id(self, id: int, session: AsyncSession) -> Base | None:
+        """ get model by id from the database
+
+        Args:
+            id (int): model id
+            session (AsyncSession): session db
+
+        Returns:
+            Base: model or none
+        """
+        pass
+
+    @abstractmethod
+    async def create(self, model: Base, session: AsyncSession) -> Base:
+        """ create model in the database
+
+        Args:
+            model (Base): creating model
+            session (AsyncSession): session db
+
+        Returns:
+            Base: created model
+        """
+        pass
+
+    @abstractmethod
+    async def update(self, model: Base, session: AsyncSession) -> Base:
+        """ update model in the database
+
+        Args:
+            model (Base): model with updated fields
+            session (AsyncSession): session db
+
+        Returns:
+            Base: updated base
+        """
+        pass
+
+    @abstractmethod
+    async def delete(self, id: int, session: AsyncSession) -> None:
+        """ deleting model from the database
+
+        Args:
+            id (int): deleting model id
+            session (AsyncSession): session db
+        """
+        pass
--- backend/source/crud/forms.py
+++ backend/source/crud/forms.py
@@ -1,11 +1,11 @@
-from sqlalchemy import Sequence, select
+from sqlalchemy import Sequence, select, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from source.schemas.form import FormRead
+from source.crud.base import BaseCRUD
 from source.models.form import Form
 from source.schemas.base import Base
 
-class FormsCrud:
+class FormsCrud(BaseCRUD):
     async def get_all(self, pagination: tuple[int, int], session: AsyncSession) -> Sequence[Base]:
         skip, limit = pagination
         query = select(Form).offset(skip).limit(limit)
@@ -13,8 +13,11 @@ class FormsCrud:
 
         return res.scalars().all()
 
-    async def get_by_id(self) -> Base:
-        pass
+    async def get_by_id(self, id: int, session: AsyncSession) -> Base | None:
+        query = select(Form).where(Form.id == id)
+        res = await session.execute(query)
+
+        return res.scalars().one_or_none()
 
     async def create(self, model: Base, session: AsyncSession) -> Base:
         new_model = Form(**model.model_dump())
@@ -23,3 +26,13 @@ class FormsCrud:
         await session.refresh(new_model)
 
         return new_model
+
+    async def update(self, model: Base, session: AsyncSession) -> Base:
+        pass
+
+    async def delete(self, id: int, session: AsyncSession) -> None:
+        query = delete(Form).where(Form.id == id)
+        await session.execute(query)
+        await session.commit()
+
+        return
