9387262bbd7907623d91a25c8e7681bbda4b4869
Kamil Fedio <kamilf827@gmail.com>
2024-08-19 16:33:23 +0200

    feature: add new db schema

--- backend/source/__init__.py
+++ backend/source/__init__.py
@@ -1,2 +1,3 @@
 from source.models.form import Form
 from source.models.base import Base
+from source.models.user import User
---
+++ backend/source/models/user.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy import DateTime, Text, String, TIMESTAMP, func
+from datetime import datetime
+
+from source.models.base import Base
+
+class User(Base):
+
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    picture_url: Mapped[str] = mapped_column(String(255), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), onupdate=func.now(), nullable=False)
