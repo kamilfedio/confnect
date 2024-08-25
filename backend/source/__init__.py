60fb8fceaf1c2b9e8fd716ae1faff0c00bc2ba9b
Kamil Fedio <kamilf827@gmail.com>
2024-08-25 14:07:55 +0200

    BE: add imports

--- backend/source/__init__.py
+++ backend/source/__init__.py
@@ -1,4 +1,5 @@
 from source.models.form import Form
 from source.models.base import Base
 from source.models.user import User
-from source.models.token import Tokens
+from source.models.token import Token
+from source.models.event import Event
