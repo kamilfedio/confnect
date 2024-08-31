6ef07b9979a48869f79968177089ce2b49a03e06
Kamil Fedio <kamilf827@gmail.com>
2024-08-31 14:42:47 +0200

    BE: add new token param

--- backend/source/models/token.py
+++ backend/source/models/token.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import mapped_column, Mapped, relationship
-from sqlalchemy import Enum, Text, DateTime, func, Integer, ForeignKey
+from sqlalchemy import Boolean, Enum, Text, DateTime, func, Integer, ForeignKey
 from datetime import datetime
 from source.models.base import Base
 from source.utils.enums import TokenType
@@ -11,6 +11,7 @@ class Token(Base):
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'), nullable=False)
     type: Mapped[TokenType] = mapped_column(Enum(TokenType), nullable=False)
     expiration_date: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    expirated: Mapped[bool] = mapped_column(Boolean, default=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now(), nullable=False)
 
     user: Mapped['User'] = relationship('User', lazy='joined')
