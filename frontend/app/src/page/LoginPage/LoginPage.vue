8c003b3bd00c602f13949812f20e6269c5ba8563
JAN KRUPA <jankrupa070@gmail.com>
2024-09-02 21:36:02 +0200

    created login and register forms

--- frontend/app/src/page/LoginPage/LoginPage.vue
+++ frontend/app/src/page/LoginPage/LoginPage.vue
@@ -2,11 +2,18 @@
   <div>
     <AuthBackground />
     <div class="loginPage">
-      <div class="loginPage__loginForm loginForm"></div>
+      <div class="loginPage__loginForm loginForm">
+        <p class="loginForm__text">Again we us. We are so glad you love us!</p>
+        <form action="sumbit" class="loginForm__form">
+          <input type="email" class="loginForm__input" placeholder="email" />
+          <input type="password" class="loginForm__input" placeholder="password" />
+          <button class="loginForm__btn">Get started</button>
+        </form>
+      </div>
       <div class="loginPage__logo logo">
         <img src="../../img/whiteBlueLogo.png" alt="" />
       </div>
-      <div class="loginPage__btn" @click="goToRegister">login</div>
+      <div class="loginPage__btn" @click="goToRegister">register</div>
       <div class="loginPage__image">
         <div class="loginPage__part"></div>
       </div>
--- frontend/app/src/page/RegisterPage/RegisterPage.vue
+++ frontend/app/src/page/RegisterPage/RegisterPage.vue
@@ -2,11 +2,25 @@
   <div>
     <AuthBackground />
     <div class="registerPage">
-      <div class="registerPage__loginForm loginForm"></div>
+      <div class="registerPage__registerForm registerForm">
+        <p class="registerForm__text">
+          NIce to see you! We hope you will find everything you need here!
+        </p>
+        <form action="sumbit" class="registerForm__form">
+          <input type="email" class="registerForm__input" placeholder="email" />
+          <input type="text" class="registerForm__input" placeholder="name" />
+          <input type="password" class="registerForm__input" placeholder="password" />
+          <div class="registerForm__checkboxContainer">
+            <input type="checkbox" class="registerForm__input" id="rules" />
+            <label for="rules" class="registerForm__label">Accept rules</label>
+          </div>
+          <button class="registerForm__btn">Get started</button>
+        </form>
+      </div>
       <div class="registerPage__logo logo">
         <img src="../../img/whiteBlueLogo.png" alt="" />
       </div>
-      <div class="registerPage__btn" @click="goToLogin">Register</div>
+      <div class="registerPage__btn" @click="goToLogin">login</div>
       <div class="registerPage__image">
         <div class="registerPage__part"></div>
       </div>
